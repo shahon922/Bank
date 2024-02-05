16bc7af33126482a28608e905ae0eb34f1b8d4ee
shahon922 <shahon922@naver.com>
2024-02-05 17:05:23 +0900

    chore : Whitdrawal.cs 삭제 withdrawal.cs 변경
    whitdrawal이 아닌 withdrawal이 되어야 함
    파일 이름만 변경
    기존 내용은 전혀 안 바뀜

--- Bank/Assets/Scripts/Btn/WithdrawalMoneyBtn.cs
+++ Bank/Assets/Scripts/Btn/WithdrawalMoneyBtn.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WhitdrawalMoneyBtn : MonoBehaviour
+public class WithdrawalMoneyBtn : MonoBehaviour
 {
     private int moveMoney = 0;
 
